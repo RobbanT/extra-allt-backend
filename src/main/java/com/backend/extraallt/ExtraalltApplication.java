25abbe147de2dc879cbf41224e9987ab77f1bce2
Roberto Tencic <roberto.tencic@outlook.com>
2024-10-12 20:14:07 +0200

    Test.

--- src/main/java/com/backend/extraallt/ExtraalltApplication.java
+++ src/main/java/com/backend/extraallt/ExtraalltApplication.java
@@ -1,13 +1,18 @@
 package com.backend.extraallt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExtraalltApplication {
 
+	@Value("${openai.api.url}")
+	static String openaiApiUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExtraalltApplication.class, args);
+		System.out.println(openaiApiUrl);
 	}
 
 }
